8b2b4cff7b0cbc70f42872a82b58e659f35abd0c
HananRisma <18523002@students.uii.ac.id>
2020-03-21 19:47:49 +0700

    first commit

---
+++ GimPong/Assets/BolaMantul.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mantul : MonoBehaviour
+{
+    public int speed = 21;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0,1) * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
