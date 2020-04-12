1fe0c8aebae42aaf0f6acdd443ea269edd645656
HananRisma <18523002@students.uii.ac.id>
2020-04-12 15:15:37 +0700

    scene

--- GimPong/Assets/BolaMantul.cs
+++ GimPong/Assets/BolaMantul.cs
@@ -2,18 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mantul : MonoBehaviour
+public class BolaMantul : MonoBehaviour
 {
-    public int speed = 21;
+    public int speed = 10;
+    public Rigidbody2D ball;
+
+    public Animator animtr;
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(0,1) * speed;
+        ball.velocity = new Vector2(-1,-1) * speed;
+        animtr.SetBool("IsMove", true);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if(ball.velocity.x > 0){ //bola ke kanan
+            ball.GetComponent<Transform>().localScale = new Vector3(28,30,1);
+
+        }else{
+             ball.GetComponent<Transform>().localScale = new Vector3(-28,30,1);
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if(other.collider.name=="rightWall" || other.collider.name=="leftWall"){
+            StartCoroutine(jeda());
+        }
+    }
+
+    IEnumerator jeda(){
+        ball.velocity = Vector2.zero;
+        ball.GetComponent<Transform>().position = Vector2.zero;
+        animtr.SetBool("IsMove", false);
+        yield return new WaitForSeconds(1);
+        ball.velocity = new Vector2(-1,-1) * speed;
+        animtr.SetBool("IsMove", true);
+      }
     }
 }
--- GimPong/Assets/bolascript.cs
+++ GimPong/Assets/bolascript.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bolascript : MonoBehaviour
+public class bolascriptt : MonoBehaviour
 {
+    public int speed=10;
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Hello Game!");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(1,-1) * speed;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+   
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if(other.collider.name=="rightWall" || other.collider.name=="leftWall"){
+            GetComponent<Transform>().position = new Vector2(0,0);
+        }
     }
 }
